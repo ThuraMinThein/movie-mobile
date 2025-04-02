a45f469cb2043643302ea0c7deb4998a0361b338
ThuraMinThein <thur7012@gmail.com>
2025-04-03 00:00:03 +0630

    routing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-5xl text-blue-500">Hello</Text>
+      <Text className="text-5xl text-dark-200">Hello</Text>
     </View>
   );
 }
